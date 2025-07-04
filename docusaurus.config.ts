a4eea7d646cf4ad7d5fe3fcdd3c53fabae477f54
Guillaume Fay <guillaume_fay@hotmail.com>
2025-07-04 10:52:06 +0200

    fix: set base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,10 +18,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://doc.adresse.data.gouv.fr",
+  url: "https://baseadressenationale.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/doc-adresse/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
