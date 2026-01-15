dbb2c07626ad163f6217bc4a47182c3759b52e47
Guillaume Fay <guillaume_fay@hotmail.com>
2026-01-15 10:21:03 +0100

    fix: update prod url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://doc.bal.dev.incubateur.anct.gouv.fr/",
+  url: "https://doc.adresse.data.gouv.fr/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
